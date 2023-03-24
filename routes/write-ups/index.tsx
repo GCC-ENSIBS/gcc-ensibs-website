58d0538bfab228a4e9b6bd0940ca88d1fe83728e
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-03-24 22:16:22 +0100

    writeups index working

--- routes/write-ups/index.tsx
+++ routes/write-ups/index.tsx
@@ -1,7 +1,8 @@
 import Header from "../../components/Header.tsx";
 import {Head} from "$fresh/src/runtime/head.ts";
+import {Handlers, PageProps} from "$fresh/src/server/types.ts";
 
-export default function WriteUps() {
+export default function WriteUps(props: PageProps) {
     return (
         <>
             <Head>
@@ -13,6 +14,35 @@ export default function WriteUps() {
                 <h1 className="text-4xl font-bold">Write-ups</h1>
                 <p className="text-xl">Here are some write-ups of CTFs we have participated in.</p>
             </div>
+            <div className={" max-w-screen-lg mx-auto p-4 my-8 flex flex-wrap"}>
+                {props.data.map((writeup: string) => {
+                        return (
+                            <div className={"w-1/2 md:w-1/3 p-4"}>
+                                <a href={`/write-ups/${writeup.replace(" ", "_")}`}
+                                   className={"block bg-gray-800 rounded-md p-4 hover:scale-110 transform transition duration-500"}>
+                                    <h2 className={"text-xl text-center font-bold"}>{writeup}</h2>
+                                </a>
+                            </div>
+                        )
+                    }
+                )}
+            </div>
+
+
         </>
     )
+}
+
+export const handler: Handlers = {
+    async GET(req, ctx) {
+        // Fetch write ups file name in write-ups folder
+        const writeups = [];
+        for await (const dirEntry of Deno.readDir('./write-ups')) {
+            if (dirEntry.isFile && dirEntry.name.endsWith(".md")) {
+                writeups.push(dirEntry.name.replace(".md", "").replace("_", " "));
+            }
+        }
+
+        return ctx.render(writeups)
+    }
 }
