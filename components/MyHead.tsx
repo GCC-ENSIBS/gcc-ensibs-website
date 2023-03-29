7508b469c6b4ccb445c16efae45a00d9650934d7
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-03-29 12:47:17 +0200

    Card

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -1,12 +1,17 @@
 import {Head} from "$fresh/src/runtime/head.ts";
 
 export default function MyHead() {
-  return (
-    <Head>
-      <title>GCC</title>
-        <meta name="description" content="Galette Cidre CTF" />
-        <link rel="stylesheet" href="/main.css" />
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
-  );
+    return (
+        <Head>
+            <title>GCC</title>
+            <meta name="description" content="Club CTF de l'ENSIBS"></meta>
+            <meta property="og:title" content="GCC - ENSIBS"></meta>
+            <meta property="og:description" content="Club CTF de l'ENSIBS"></meta>
+            <meta property="og:image" content="/back.png"></meta>
+            <meta property="og:url" content="/"></meta>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <link rel="stylesheet" href="/main.css"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+    );
 }
